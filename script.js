f911e5f0154924201ca62f5be658d485603cad82
Simeon Rediger <simeonrediger@gmail.com>
2025-04-27 23:37:17 -0400

    Shade on hover and add drawing with mouse events

--- script.js
+++ script.js
@@ -2,9 +2,32 @@ const grid = document.querySelector('#grid');
 const gridSize = 16;
 populateGrid();
 
+let leftMouseIsDown;
+trackLeftMouseClicks();
+
+
 function populateGrid() {
     for (let i = 0; i < gridSize ** 2; i++) {
         const pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+
+        pixel.addEventListener('mousedown', draw);
+        pixel.addEventListener('mouseenter', event => {
+            if (leftMouseIsDown) {
+                draw(event);
+            }
+        });
+
         grid.append(pixel);
     }
+}
+
+function trackLeftMouseClicks() {
+    document.body.addEventListener('mousedown', () => leftMouseIsDown = true);
+    document.body.addEventListener('mouseup', () => leftMouseIsDown = false);
+}
+
+function draw(event) {
+    const pixel = event.target;
+    pixel.classList.add('filled');
 }
